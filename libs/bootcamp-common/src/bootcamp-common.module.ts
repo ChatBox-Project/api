7b252dc9182491a468e28ac5c8c3145ab300defe
daenv <daenvle69@gmail.com>
2024-02-22 23:15:50 +0700

    Delete unused files

--- libs/bootcamp-common/src/bootcamp-common.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BootcampCommonService } from './bootcamp-common.service';
-
-@Module({
-  providers: [BootcampCommonService],
-  exports: [BootcampCommonService],
-})
-export class BootcampCommonModule {}
