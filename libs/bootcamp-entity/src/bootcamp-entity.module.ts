27460bb1b8494abc3254d7751a7a7a9100b53bb9
daenv <daenvle69@gmail.com>
2024-02-22 23:34:36 +0700

    Remove BootcampEntityModule and BootcampEntityService files

--- libs/bootcamp-entity/src/bootcamp-entity.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BootcampEntityService } from './bootcamp-entity.service';
-
-@Module({
-  providers: [BootcampEntityService],
-  exports: [BootcampEntityService],
-})
-export class BootcampEntityModule {}
