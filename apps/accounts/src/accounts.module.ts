36ea3af63f8828e92a4966363062712991369421
daenv <daenvle69@gmail.com>
2024-02-22 23:25:38 +0700

    Add ConfigModule and DatabaseModule imports

--- apps/accounts/src/accounts.module.ts
+++ apps/accounts/src/accounts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/bootcamp-common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
