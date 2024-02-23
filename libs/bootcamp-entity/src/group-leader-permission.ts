fa650c48520ee7b0c0775bccaf630012cecb363e
daenv <daenvle69@gmail.com>
2024-02-23 23:32:16 +0700

    Add GroupLeaderPermission class

---
+++ libs/bootcamp-entity/src/group-leader-permission.ts
@@ -0,0 +1,7 @@
+export class GroupLeaderPermission {
+  public id?: string;
+  public groupId?: string;
+  public leaderId?: string;
+  public permissionId?: string;
+  public group_leader_permissionId?: string;
+}
