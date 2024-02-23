55e184996b12d2e8cfa4e845f34c37106acdd25b
daenv <daenvle69@gmail.com>
2024-02-23 18:16:19 +0700

    Add AuthController, AuthService, and AuthModule

---
+++ apps/auth/src/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  getHello(): string {
+    return this.authService.getHello();
+  }
+
+  @Post()
+  test(@Body() body: any) {
+    console.log(body);
+  }
+}
