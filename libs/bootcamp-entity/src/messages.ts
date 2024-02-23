4df822b77b01cf0e58e4357741af7e661b81254a
daenv <daenvle69@gmail.com>
2024-02-23 23:50:25 +0700

    Add Messages class to bootcamp-entity

---
+++ libs/bootcamp-entity/src/messages.ts
@@ -0,0 +1,11 @@
+export class Messages {
+  public messageId?: string;
+  public chatboxId?: string;
+  public senderId?: string;
+  public messageType?: string;
+  public contentText?: string;
+  public contentImage?: string;
+  public contentAudio?: string;
+  public contentVideo?: string;
+  public contentFile?: string;
+}
